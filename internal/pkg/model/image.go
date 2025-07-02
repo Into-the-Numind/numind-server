ccab9a11e71029836919c1d2d96d44fb56fb039a
OctopusLian <zoctopus@qq.com>
2025-07-02 10:51:13 +0800

    :sparkles: update image model

--- internal/pkg/model/image.go
+++ internal/pkg/model/image.go
@@ -16,10 +16,11 @@ type ImageM struct {
 	Width       int    `json:"width"`                                    // 图片宽度
 	Height      int    `json:"height"`                                   // 图片高度
 	Status      string `gorm:"size:20;default:'uploaded'" json:"status"` // 状态: uploaded, processing, processed, failed
-	ProcessMsg  string `gorm:"size:255" json:"process_msg"`              // 处理信息
+	ProcessMsg  string `gorm:"type:text" json:"process_msg"`             // 处理信息
 
 	// 关联关系
-	User User `gorm:"foreignKey:UserID" json:"user,omitempty"`
+	User  User    `gorm:"foreignKey:UserID" json:"user,omitempty"`
+	Cards []CardM `gorm:"many2many:card_rel_image;joinForeignKey:ImageID;JoinReferences:CardID" json:"cards,omitempty"`
 }
 
 func (ImageM) TableName() string {
