bb8f299c9102d0044d0fb313713731bc1881880b
OctopusLian <zoctopus@qq.com>
2025-08-12 17:09:52 +0800

    ♻️ user CRUD api

--- internal/numind/router.go
+++ internal/numind/router.go
@@ -194,6 +194,12 @@ func installNumindRouters(g *gin.Engine) error {
 		adminGroup.PUT("/categories/:id", adminc.UpdateCategory)            // 更新分类
 		adminGroup.DELETE("/categories/:id", adminc.DeleteCategory)         // 删除分类
 		adminGroup.GET("/stats", adminc.GetStats)                           // 获取统计信息
+
+		// 用户管理相关API（管理员权限）
+		adminGroup.GET("/users", uc.List)            // 查询用户列表
+		adminGroup.GET("/users/:name", uc.Get)       // 查询用户详情
+		adminGroup.PUT("/users/:name", uc.Update)    // 更改用户
+		adminGroup.DELETE("/users/:name", uc.Delete) // 删除用户
 	}
 
 	// 配置相关（管理员权限）
@@ -209,13 +215,9 @@ func installNumindRouters(g *gin.Engine) error {
 
 	// 用户相关
 	{
-		//authGroup.GET("/users", uc.List)              // 查询用户列表
 		authGroup.GET("/users/me", uc.GetCurrentUser)    // 获取当前用户信息
 		authGroup.PUT("/users/me", uc.UpdateProfile)     // 更新当前用户个人信息
 		authGroup.POST("/users/avatar", uc.UploadAvatar) // 上传用户头像
-		//authGroup.GET("/users/:name", uc.Get)         // 查询用户详情
-		//authGroup.PUT("/users/:name", uc.Update)      // 更改用户
-		//authGroup.DELETE("/users/:name", uc.Delete)   // 删除用户
 	}
 
 	// 微信支付相关
