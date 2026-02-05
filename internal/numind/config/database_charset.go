f9bc6f6ef3c55153d82336d08ed73644e53433ed
pmtmyaggy <pmtmyaggy@gmail.com>
2026-02-05 12:24:35 +0800

    fix(db): add sales and wecom tables to charset verification list

--- internal/numind/config/database_charset.go
+++ internal/numind/config/database_charset.go
@@ -46,6 +46,11 @@ func DefaultDatabaseCharsetConfig() *DatabaseCharsetConfig {
 			"account_record",
 			"knowledge_document",
 			"knowledge_chunk",
+			"sales_session",
+			"sales_message",
+			"language_style",
+			"wecom_user",
+			"wecom_message",
 		},
 	}
 }
